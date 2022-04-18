1dbf2e7f4761b3160eb73f42309eeab43cc5ab37
Jacob <maimaisatt@gmail.com>
2022-04-18 09:11:25 +0800

    First commit

---
+++ popcat.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+driver = webdriver.Chrome("/Users/kaohuiling1108/Documents/popcat/chromedriver")
+driver.get("https://popcat.click/?fbclid=IwAR3CW5KILkvfCSsAysw7jILWKYZNgeV4Fp28555fHEcvGMef_owLexBAbNM")
+#time.sleep(5)
+driver.implicitly_wait(10)
+ele = driver.find_element_by_css_selector(".cat-img")
+for i in range(9999999999999999999999999999999999999999999999):
+    if i < 9999999999999999999999999999999999999999999999:
+        ele.click()
+
